ea707c02b70c0436352b04f11911d2211a94d257
Hunter Oatway <hunter@immortal.local>
2021-03-03 20:42:08 -0600

    added basic dotnet mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace opentrek.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
