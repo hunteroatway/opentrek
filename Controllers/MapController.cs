31e70ebf5d421bc98b2c1575c3b6b78151b3df03
Hunter Oatway <hunter@immortal.local>
2021-03-03 21:11:25 -0600

    add map controller for displaying map

---
+++ Controllers/MapController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using opentrek.Models;
+
+namespace opentrek.Controllers
+{
+    public class MapController : Controller
+    {
+        private readonly ILogger<MapController> _logger;
+
+        public MapController(ILogger<MapController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
