e46c2f10c3bc4343307b8767d738c33db0cf85e4
Hunter Oatway <hunter@immortal.local>
2021-03-13 23:37:30 -0600

    remove draggable marker, call locationiq api and parse json reponse to get country

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,11 +4,12 @@ var map = new mapboxgl.Map({
   container: 'map',
   attributionControl: false,
   style: 'https://tiles.locationiq.com/v3/streets/vector.json?key=' + config.location_iq_api_key,
-  zoom: 8,
-  center: [-122.42, 37.779]
+  zoom: 1,
+  center: [0, 25.0]
 });
 
 // create an element to hold the marker
+var marker;
 var marker_el = document.createElement('div');
 marker_el.className = 'marker';
 marker_el.style.backgroundImage = 'url(https://maps.locationiq.com/v3/samples/marker50px.png)';
@@ -16,29 +17,38 @@ marker_el.style.width = '50px';
 marker_el.style.height = '50px';
 
 // define a new mapboxgl marker object, set the position and add it to the map object
-var marker = new mapboxgl.Marker({
+marker = new mapboxgl.Marker({
   element: marker_el,
   draggable: true
-}).setLngLat([0, 0]).addTo(map);
+}).setLngLat([0, 25.0]).addTo(map);
 
-function updateMarkerPos(e) {
-  // update position of marker on map to current event lat and lng values
+function addMarker(e) {
+  // define a new mapboxgl marker object, set the position and add it to the map object
   marker = new mapboxgl.Marker({
     element: marker_el,
     draggable: true
   }).setLngLat(e.lngLat.wrap()).addTo(map);
 
-  getLatLng();
+  // call api to get country of location that was clicked
+  getCountryFromLatLng();
 }
 
-// get the lat and lng at the end of a drag event
-function getLatLng() {
-  // get the markers lat, lng coordinates and update the pre-defined dom object to display it to the user
-  var lngLat = marker.getLngLat();
-  coordinates.style.display = 'block';
-  coordinates.innerHTML = 'Latitude: ' + lngLat.lat + '<br />Longitude: ' + lngLat.lng;
+// function to call api to get country
+function getCountryFromLatLng() {
+  const req = new XMLHttpRequest();
+  const url = 'https://us1.locationiq.com/v1/reverse.php?key=' + config.location_iq_api_key + '&lat=' + marker.getLngLat().lat + '&lon=' + marker.getLngLat().lng + '&format=json';
+
+  // send http get request and parse json reponse to get the country
+  req.onreadystatechange = function test() {
+    if (this.readyState == 4 && this.status == 200) {
+      var json = JSON.parse(this.responseText);
+      document.getElementById("country").innerHTML = json.address.country;
+    }
+  }  
+  req.open("GET", url);
+  req.send();
 }
 
 // setup event listeners for map and marker
-map.on('click', updateMarkerPos);
-marker.on('dragend', getLatLng);
+map.on('click', addMarker);
+marker.on('dragend', addMarker);
