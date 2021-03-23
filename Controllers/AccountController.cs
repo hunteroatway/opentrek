cae99ceaade6a842d52c900a533264de2b19a16e
hunteroatway <oatway.h@gmail.com>
2021-03-23 17:21:18 -0600

    add login/signup page layouts

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opentrek.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
