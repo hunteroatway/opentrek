deb60c0eab66dbfd747a8657359b9dce3f3b5313
Hunter Oatway <hunter@immortal.local>
2021-04-06 21:53:08 -0600

    add model validation

--- Models/LocationModel.cs
+++ Models/LocationModel.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace opentrek.Models
 {
     public class LocationModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Invalid Country Format")]
         public string Country { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Invalid Recommendation Format")]
         public string Recommendation { get; set; }
     }
 }
--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace opentrek.Models
 {
     public class UserModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "Invalid First Name Length")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "Invalid Last Name Length")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "Invalid Username Length")]
         public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", ErrorMessage = "Invalid Email Format")]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", ErrorMessage = "Invalid Password Format")]
         public string Password { get; set; }
     }
 }
